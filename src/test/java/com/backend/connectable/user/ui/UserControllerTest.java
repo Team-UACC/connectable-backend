89c7733f560da89d215c28c73c076fcdb9100459
mrlee7 <jplee227@gmail.com>
2022-07-05 16:27:16 +0900

    style: import 정리

--- src/test/java/com/backend/connectable/user/ui/UserControllerTest.java
+++ src/test/java/com/backend/connectable/user/ui/UserControllerTest.java
@@ -10,10 +10,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
