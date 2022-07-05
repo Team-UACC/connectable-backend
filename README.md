1289628b5cff8aa6b7d643388ecfc7ecb53851fc
joelonsw <joel610naver@gmail.com>
2022-07-05 10:54:10 +0900

    feat: 프로젝트 셋업

---
+++ README.md
@@ -0,0 +1,1 @@
+## Connectable Backend
---
+++ src/main/java/com/backend/connectable/ConnectableApplication.java
@@ -0,0 +1,13 @@
+package com.backend.connectable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectableApplication.class, args);
+    }
+
+}
