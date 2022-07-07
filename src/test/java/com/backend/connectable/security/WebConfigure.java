e0b61e4db2f193ca45561d1e472cca2e30a32041
mrlee7 <jplee227@gmail.com>
2022-07-07 17:57:29 +0900

    feat: Cors 이슈 수정 (#7)
    * feat: 크로스도메인 이슈 해결
    
    * feat: 크로스도메인 이슈 수정

---
+++ src/test/java/com/backend/connectable/security/WebConfigure.java
@@ -0,0 +1,16 @@
+package com.backend.connectable.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowCredentials(true);
+    }
+}
