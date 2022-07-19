6f8c6849c39218638240d1bb81c51deb3e33e1f1
mrlee7 <jplee227@gmail.com>
2022-07-19 14:23:58 +0900

    fix: 닉네임 검증로직 및 문구 수정

--- src/main/java/com/backend/connectable/user/ui/dto/UserModifyRequest.java
+++ src/main/java/com/backend/connectable/user/ui/dto/UserModifyRequest.java
@@ -18,8 +18,8 @@ public class UserModifyRequest {
 
     @NotBlank(message = "닉네임은 필수 입력값 입니다.",
         groups = NotEmptyGroup.class)
-    @Pattern(regexp = "^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣]{2,20}$",
-        message = "닉네임은 2자 이상 20자 이하, 영어 또는 숫자 또는 한글로 구성된 닉네임이여야 합니다.",
+    @Pattern(regexp = "^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣]{2,10}$",
+        message = "닉네임은 2자 이상 10자 이하, 영어 또는 숫자 또는 한글로 구성된 닉네임이여야 합니다.",
         groups = PatternCheckGroup.class)
     private String nickname;
 
