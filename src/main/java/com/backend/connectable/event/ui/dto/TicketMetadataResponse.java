ff73674ed6d953085a80a22221d21fbb9fc26b2a
joelonsw <joel610naver@gmail.com>
2022-07-19 11:22:03 +0900

    fix: 티켓 메타데이터 dto 도입

---
+++ src/main/java/com/backend/connectable/event/ui/dto/TicketMetadataResponse.java
@@ -0,0 +1,29 @@
+package com.backend.connectable.event.ui.dto;
+
+import com.backend.connectable.event.domain.TicketMetadata;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketMetadataResponse {
+
+    private String name;
+    private String description;
+    private String image;
+    private List<TicketMetadataAttributeResponse> attributes;
+
+    public static TicketMetadataResponse of(TicketMetadata metadata) {
+        return new TicketMetadataResponse(
+                metadata.getName(),
+                metadata.getDescription(),
+                metadata.getImage(),
+                TicketMetadataAttributeResponse.toList(metadata.getAttributes())
+        );
+    }
+}
