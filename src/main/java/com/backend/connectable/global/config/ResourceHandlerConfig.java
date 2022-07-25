9cc7efc8a952b4612004724f498f6d5dcd694f47
joelonsw <joel610naver@gmail.com>
2022-07-25 15:01:54 +0900

    refactor: global/config 패키지로 이동

--- src/main/java/com/backend/connectable/global/config/ResourceHandlerConfig.java
+++ src/main/java/com/backend/connectable/global/config/ResourceHandlerConfig.java
@@ -1,4 +1,4 @@
-package com.backend.connectable;
+package com.backend.connectable.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer {
+public class ResourceHandlerConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/backend/connectable/global/config/SwaggerConfig.java
+++ src/main/java/com/backend/connectable/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.backend.connectable;
+package com.backend.connectable.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
