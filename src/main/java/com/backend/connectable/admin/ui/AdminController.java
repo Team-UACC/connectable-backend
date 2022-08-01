cb9d1fdbdf320fb034863bcdd6658ebb602dedba
joelonsw <joel610naver@gmail.com>
2022-08-01 16:27:28 +0900

    feat: Admin 엔드포인트 작성

---
+++ src/main/java/com/backend/connectable/admin/ui/AdminController.java
@@ -0,0 +1,35 @@
+package com.backend.connectable.admin.ui;
+
+import com.backend.connectable.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @PutMapping("/order-details/{order-detail-id}/paid")
+    public ResponseEntity<Void> orderDetailToPaid(@PathVariable("order-detail-id") Long orderDetailId) {
+        adminService.orderDetailToPaid(orderDetailId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/order-details/{order-detail-id}/unpaid")
+    public ResponseEntity<Void> orderDetailToUnpaid(@PathVariable("order-detail-id") Long orderDetailId) {
+        adminService.orderDetailToUnpaid(orderDetailId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/order-details/{order-detail-id}/refund")
+    public ResponseEntity<Void> orderDetailToRefund(@PathVariable("order-detail-id") Long orderDetailId) {
+        adminService.orderDetailToRefund(orderDetailId);
+        return ResponseEntity.ok().build();
+    }
+}
