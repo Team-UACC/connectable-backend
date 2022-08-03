65a804f1e27ca97c6627473cb8998b49173300c2
mrlee7 <jplee227@gmail.com>
2022-08-03 15:08:35 +0900

    fix: scheduledTask Component 등록

--- src/main/java/com/backend/connectable/schedule/ScheduledTasks.java
+++ src/main/java/com/backend/connectable/schedule/ScheduledTasks.java
@@ -4,7 +4,9 @@ import com.backend.connectable.event.domain.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduledTasks {
