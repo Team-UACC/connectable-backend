c3681c805c41ed5cd63258cacd5ac18aad2fe86b
Yeongsang Jo <61370901+joelonsw@users.noreply.github.com>
2022-08-05 18:07:18 +0900

    fix: get user 로직 수정 (#70)

--- src/main/java/com/backend/connectable/user/ui/dto/UserResponse.java
+++ src/main/java/com/backend/connectable/user/ui/dto/UserResponse.java
@@ -12,6 +12,9 @@ import lombok.Setter;
 @Setter
 public class UserResponse {
 
+    private final static String STATUS_SUCCESS = "success";
+    private final static String STATUS_FAILURE = "failure";
+
     private String status;
     private String nickname;
     private String phoneNumber;
@@ -24,5 +27,27 @@ public class UserResponse {
                 user.getPhoneNumber(),
                 user.getKlaytnAddress()
         );
+    }
+
+    public static UserResponse ofSuccess(User user) {
+        return new UserResponse(
+            STATUS_SUCCESS,
+            user.getNickname(),
+            user.getPhoneNumber(),
+            user.getKlaytnAddress()
+        );
+    }
+
+    public static UserResponse ofFailure(User user) {
+        return new UserResponse(
+            STATUS_FAILURE,
+            user.getNickname(),
+            user.getPhoneNumber(),
+            user.getKlaytnAddress()
+        );
+    }
+
+    public boolean isSuccess() {
+        return this.status.equals(STATUS_SUCCESS);
     }
 }
