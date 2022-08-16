d8c41c573276bf1fef48910416ad7a3520fc172e
joelonsw <joel610naver@gmail.com>
2022-08-16 14:26:12 +0900

    refactor(OrderService): 서비스 내에서 user를 조회하도록 수정

--- src/main/java/com/backend/connectable/order/service/OrderService.java
+++ src/main/java/com/backend/connectable/order/service/OrderService.java
@@ -2,6 +2,8 @@ package com.backend.connectable.order.service;
 
 import com.backend.connectable.event.domain.Ticket;
 import com.backend.connectable.event.domain.repository.TicketRepository;
+import com.backend.connectable.exception.ConnectableException;
+import com.backend.connectable.exception.ErrorType;
 import com.backend.connectable.order.domain.Order;
 import com.backend.connectable.order.domain.OrderDetail;
 import com.backend.connectable.order.domain.OrderStatus;
@@ -13,7 +15,9 @@ import com.backend.connectable.order.ui.dto.OrderRequest;
 import com.backend.connectable.order.ui.dto.OrderResponse;
 import com.backend.connectable.security.ConnectableUserDetails;
 import com.backend.connectable.user.domain.User;
+import com.backend.connectable.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,24 +29,30 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class OrderService {
 
+    private final UserRepository userRepository;
     private final OrderRepository orderRepository;
     private final TicketRepository ticketRepository;
 
     @Transactional
     public OrderResponse createOrder(ConnectableUserDetails userDetails, OrderRequest orderRequest) {
-        User user = userDetails.getUser();
+        User user = findUser(userDetails.getKlaytnAddress());
         Order order = generateOrder(user, orderRequest);
         orderRepository.save(order);
         return OrderResponse.from("success");
     }
 
     public List<OrderDetailResponse> getOrderDetailList(ConnectableUserDetails userDetails) {
-        String klaytnAddress = userDetails.getUser().getKlaytnAddress();
+        String klaytnAddress = userDetails.getKlaytnAddress();
         List<TicketOrderDetail> orderDetailResponses = orderRepository.getOrderDetailList(klaytnAddress);
 
         return orderDetailResponses.stream()
             .map(OrderMapper.INSTANCE::ticketOrderDetailToResponse)
             .collect(Collectors.toList());
+    }
+
+    private User findUser(String klaytnAddress) {
+        return userRepository.findByKlaytnAddress(klaytnAddress)
+            .orElseThrow(() -> new ConnectableException(HttpStatus.BAD_REQUEST, ErrorType.USER_NOT_FOUND));
     }
 
     private Order generateOrder(User user, OrderRequest orderRequest) {
