751fdb836a4314a4095b930d7ab328c5954fe729
joelonsw <joel610naver@gmail.com>
2022-08-16 17:24:39 +0900

    feat(JwtAuthenticationFilter): try-catch 추가

--- src/main/java/com/backend/connectable/security/JwtAuthenticationFilter.java
+++ src/main/java/com/backend/connectable/security/JwtAuthenticationFilter.java
@@ -1,7 +1,9 @@
 package com.backend.connectable.security;
 
+import com.backend.connectable.security.exception.ConnectableSecurityException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -27,13 +29,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
         log.info("[[Token]] : " + token);
         log.info("[[Path]] : " + path);
-        if (!Objects.isNull(token)) {
-            verifyTokenAccordingToPath(token, path);
+        try {
+            if (!Objects.isNull(token)) {
+                verifyTokenAccordingToPath(token, path);
+            }
+            filterChain.doFilter(request, response);
+        } catch (ConnectableSecurityException e) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(e.getMessage());
         }
-        filterChain.doFilter(request, response);
     }
 
-    private void verifyTokenAccordingToPath(String token, String path) {
+    private void verifyTokenAccordingToPath(String token, String path) throws ConnectableSecurityException {
         if (path.startsWith("/admin")) {
             jwtProvider.verifyAdmin(token);
             return;
