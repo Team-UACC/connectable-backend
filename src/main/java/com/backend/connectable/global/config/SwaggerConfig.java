7674feace558277af6a5be31e278d8f4fb81171e
joelonsw <joel610naver@gmail.com>
2022-08-18 17:14:42 +0900

    feat: swagger 설정

--- src/main/java/com/backend/connectable/global/config/SwaggerConfig.java
+++ src/main/java/com/backend/connectable/global/config/SwaggerConfig.java
@@ -1,23 +1,53 @@
 package com.backend.connectable.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.function.Predicate;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.url}")
+    private String baseUrl;
+
     @Bean
-    public Docket api() {
+    public Docket parseApi() {
         return new Docket(DocumentationType.OAS_30)
             .ignoredParameterTypes(AuthenticationPrincipal.class)
+            .servers(serverInfo())
+            .apiInfo(apiInfo())
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.backend.connectable"))
             .paths(PathSelectors.ant("/**"))
+            .paths(Predicate.not(PathSelectors.regex("/admin.*")))
+            .build();
+    }
+
+    private Server serverInfo() {
+        return new Server("",
+            baseUrl,
+            "",
+            Collections.emptyList(),
+            Collections.emptyList()
+        );
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Connectable API Documentation")
+            .description("API documentation of Connectable project by Team UACC")
+            .version("1.0.0")
             .build();
     }
 }
--- src/main/java/com/backend/connectable/user/ui/UserController.java
+++ src/main/java/com/backend/connectable/user/ui/UserController.java
@@ -4,6 +4,7 @@ import com.backend.connectable.exception.sequence.ValidationSequence;
 import com.backend.connectable.security.ConnectableUserDetails;
 import com.backend.connectable.user.service.UserService;
 import com.backend.connectable.user.ui.dto.*;
+import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,7 @@ public class UserController {
     }
 
     @GetMapping
+    @ApiImplicitParam(name = "Authorization", paramType = "header", required = true)
     public ResponseEntity<UserResponse> getUser(@AuthenticationPrincipal ConnectableUserDetails userDetails) {
         UserResponse userResponse = userService.getUserByUserDetails(userDetails);
         if (userResponse.checkSuccess()) {
@@ -37,12 +39,14 @@ public class UserController {
     }
 
     @DeleteMapping
+    @ApiImplicitParam(name = "Authorization", paramType = "header", required = true)
     public ResponseEntity<UserModifyResponse> deleteUser(@AuthenticationPrincipal ConnectableUserDetails userDetails) {
         UserModifyResponse userModifyResponse = userService.deleteUserByUserDetails(userDetails);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(userModifyResponse);
     }
 
     @PutMapping
+    @ApiImplicitParam(name = "Authorization", paramType = "header", required = true)
     public ResponseEntity<UserModifyResponse> modifyUser(@AuthenticationPrincipal ConnectableUserDetails userDetails,
                                                          @RequestBody @Validated(ValidationSequence.class) UserModifyRequest userModifyRequest) {
         UserModifyResponse userModifyResponse = userService.modifyUserByUserDetails(userDetails, userModifyRequest);
@@ -50,6 +54,7 @@ public class UserController {
     }
 
     @GetMapping("/tickets")
+    @ApiImplicitParam(name = "Authorization", paramType = "header", required = true)
     public ResponseEntity<UserTicketListResponse> getUserTickets(@AuthenticationPrincipal ConnectableUserDetails userDetails) {
         UserTicketListResponse userTicketListResponse = userService.getUserTicketsByUserDetails(userDetails);
         return ResponseEntity.ok(userTicketListResponse);
