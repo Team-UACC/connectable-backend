4bc1f62797c68f3dee18eef7b6e68ce740a88ed8
joelonsw <joel610naver@gmail.com>
2022-08-19 10:13:36 +0900

    refactor: ApiImplicitParam 어노테이션 제거

--- src/main/java/com/backend/connectable/order/ui/OrderController.java
+++ src/main/java/com/backend/connectable/order/ui/OrderController.java
@@ -6,7 +6,6 @@ import com.backend.connectable.order.ui.dto.OrderDetailResponse;
 import com.backend.connectable.order.ui.dto.OrderRequest;
 import com.backend.connectable.order.ui.dto.OrderResponse;
 import com.backend.connectable.security.ConnectableUserDetails;
-import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -27,7 +26,6 @@ public class OrderController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping
-    @ApiImplicitParam(name = "Authorization", paramType = "header", required = true)
     public ResponseEntity<OrderResponse> createOrder(
         @AuthenticationPrincipal ConnectableUserDetails userDetails,
         @RequestBody @Validated(ValidationSequence.class) OrderRequest request
@@ -38,7 +36,6 @@ public class OrderController {
     }
 
     @GetMapping("/list")
-    @ApiImplicitParam(name = "Authorization", paramType = "header", required = true)
     public ResponseEntity<List<OrderDetailResponse>> getOrderDetailList(
         @AuthenticationPrincipal ConnectableUserDetails userDetails
     ) {
