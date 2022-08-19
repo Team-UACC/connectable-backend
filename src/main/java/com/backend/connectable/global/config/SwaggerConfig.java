0fc06b74622f7210543f050612d6fa0e5c6a831d
Yeongsang Jo <61370901+joelonsw@users.noreply.github.com>
2022-08-19 10:27:03 +0900

    refactor: swagger 설정 변경 (#81)
    * feat: swagger 설정
    
    * refactor: ApiImplicitParam 어노테이션 제거
    
    * feat: 전역 헤더 요청 값 추가

--- src/main/java/com/backend/connectable/global/config/SwaggerConfig.java
+++ src/main/java/com/backend/connectable/global/config/SwaggerConfig.java
@@ -1,23 +1,73 @@
 package com.backend.connectable.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.url}")
+    private String baseUrl;
+
     @Bean
-    public Docket api() {
+    public Docket parseApi() {
         return new Docket(DocumentationType.OAS_30)
             .ignoredParameterTypes(AuthenticationPrincipal.class)
+            .servers(serverInfo())
+            .apiInfo(apiInfo())
+            .globalRequestParameters(parameters())
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.backend.connectable"))
             .paths(PathSelectors.ant("/**"))
+            .paths(Predicate.not(PathSelectors.regex("/admin.*")))
+            .build();
+    }
+
+    private List<RequestParameter> parameters() {
+        return singletonList(
+            new RequestParameterBuilder()
+                .name("Authorization")
+                .description("Required for user-based requests")
+                .in(ParameterType.HEADER)
+                .required(false)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .build()
+        );
+    }
+
+    private Server serverInfo() {
+        return new Server("",
+            baseUrl,
+            "",
+            Collections.emptyList(),
+            Collections.emptyList()
+        );
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Connectable API Documentation")
+            .description("API documentation of Connectable project by Team UACC")
+            .version("1.0.0")
             .build();
     }
 }
