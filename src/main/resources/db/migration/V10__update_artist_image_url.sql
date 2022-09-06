e9fafc855e803d658cfd5be5c0bccba01f09f1b7
mrlee7 <jplee227@gmail.com>
2022-09-06 11:29:57 +0900

    fix: 이미지 url 변경_Retry

--- src/main/resources/db/migration/V10__update_artist_image_url.sql
+++ src/main/resources/db/migration/V10__update_artist_image_url.sql
@@ -1,4 +1,4 @@
 UPDATE artist
 SET artist_image = "https://connectable-events.s3.ap-northeast-2.amazonaws.com/welcome-ticket/welcome-event.png"
-WHERE artist_image = "https://connectable-events.s3.ap-northeast-2.amazonaws.com/lets-rock-festival/image.jpeg"
+WHERE artist_image = "https://connectable-events.s3.ap-northeast-2.amazonaws.com/test-welcome-ticket/welcome-ticket.png"
 ;
