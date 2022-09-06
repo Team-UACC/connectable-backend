764a39c87940e289b954e94a067e3b5639206307
mrlee7 <jplee227@gmail.com>
2022-09-06 10:23:22 +0900

    feat: new artist for letslock

---
+++ src/main/resources/db/migration/V7__insert_letslock_artist.sql
@@ -0,0 +1,19 @@
+INSERT INTO artist(
+                    artist_image
+                  , artist_name
+                  , bank_account
+                  , bank_company
+                  , email
+                  , password
+                  , phone_number
+)
+    VALUE (
+    "https://connectable.fans/_next/image?url=%2Fimages%2Fwelcome2.PNG&w=640&q=75"
+    , "letslock"
+    , "3333-03-9223680"
+    , "카카오뱅크"
+    , "hyunee169@sogang.ac.kr"
+    , "sharecommon1!"
+    , "010-5248-4170"
+)
+;
