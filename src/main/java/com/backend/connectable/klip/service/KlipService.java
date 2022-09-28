85908f1dd0a3868ef14b5a454c304cc436b1f44a
joelonsw <joel610naver@gmail.com>
2022-09-28 11:11:25 +0900

    test: Klip 테스트 작성

--- src/main/java/com/backend/connectable/klip/service/KlipService.java
+++ src/main/java/com/backend/connectable/klip/service/KlipService.java
@@ -1,29 +1,31 @@
 package com.backend.connectable.klip.service;
 
+import com.backend.connectable.klip.config.KlipRestTemplate;
 import com.backend.connectable.klip.service.dto.KlipAuthHandleResponse;
 import com.backend.connectable.klip.service.dto.KlipAuthLoginResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class KlipService {
 
-    private static final String KLIP_AUTH_LOGIN_URL = "https://a2a-api.klipwallet.com/v2/a2a/result?request_key=";
+    private static final String KLIP_AUTH_LOGIN_URL =
+            "https://a2a-api.klipwallet.com/v2/a2a/result?request_key=";
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final KlipRestTemplate klipRestTemplate;
 
     public KlipAuthLoginResponse authLogin(String requestKey) {
         String requestUrl = KLIP_AUTH_LOGIN_URL + requestKey;
         try {
             KlipAuthHandleResponse klipAuthHandleResponse =
-                    restTemplate.getForEntity(requestUrl, KlipAuthHandleResponse.class).getBody();
+                    klipRestTemplate.getForEntity(requestUrl, KlipAuthHandleResponse.class);
             if (klipAuthHandleResponse.isPrepared()) {
                 return KlipAuthLoginResponse.ofPrepared();
             }
-            String klaytnAddress = klipAuthHandleResponse.getResult()
-                    .getKlaytn_address()
-                    .toLowerCase();
+            String klaytnAddress =
+                    klipAuthHandleResponse.getResult().getKlaytn_address().toLowerCase();
             return KlipAuthLoginResponse.ofCompleted(klaytnAddress);
         } catch (RestClientException | NullPointerException e) {
             return KlipAuthLoginResponse.ofFailed();
