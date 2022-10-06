553a11f4db938ad92790f0293915baf99e1d1ac3
joelonsw <joel610naver@gmail.com>
2022-10-06 09:35:48 +0900

    refactor: restTemplate 공통 구현

--- src/main/java/com/backend/connectable/s3/service/S3Service.java
+++ src/main/java/com/backend/connectable/s3/service/S3Service.java
@@ -1,15 +1,17 @@
 package com.backend.connectable.s3.service;
 
 import com.backend.connectable.event.ui.dto.TicketMetadataResponse;
+import com.backend.connectable.global.network.RestTemplateClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class S3Service {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplateClient restTemplateClient;
 
     public TicketMetadataResponse fetchMetadata(String tokenUri) {
-        return restTemplate.getForEntity(tokenUri, TicketMetadataResponse.class).getBody();
+        return restTemplateClient.getForObject(tokenUri, TicketMetadataResponse.class);
     }
 }
