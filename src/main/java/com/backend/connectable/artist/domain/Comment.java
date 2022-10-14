4ab3fc55041aefbe61e271ae93b48ddcd103811e
jungpil98 <jungpil98@ajou.ac.kr>
2022-10-14 15:10:37 +0900

    feat(Comment): DDL 스크립트 작성

--- src/main/java/com/backend/connectable/artist/domain/Comment.java
+++ src/main/java/com/backend/connectable/artist/domain/Comment.java
@@ -2,10 +2,11 @@ package com.backend.connectable.artist.domain;
 
 import com.backend.connectable.global.entity.BaseEntity;
 import com.backend.connectable.user.domain.User;
+import java.util.Objects;
+import javax.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
@@ -24,6 +25,26 @@ public class Comment extends BaseEntity {
     @JoinColumn(nullable = false)
     private User user;
 
-    @Lob
-    private String contents;
+    @Lob private String contents;
+
+    @Builder
+    public Comment(Long id, Artist artist, User user, String contents) {
+        this.id = id;
+        this.artist = artist;
+        this.user = user;
+        this.contents = contents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/resources/db/migration/V11__create_artist_comment_table.sql
+++ src/main/resources/db/migration/V11__create_artist_comment_table.sql
@@ -1,14 +1,19 @@
--- CREATE TABLE `user` (
---                         `id` bigint NOT NULL AUTO_INCREMENT,
---                         `is_active` bit(1) NOT NULL,
---                         `klaytn_address` varchar(255) DEFAULT NULL,
---                         `nickname` varchar(255) DEFAULT NULL,
---                         `phone_number` varchar(255) DEFAULT NULL,
---                         `privacy_agreement` bit(1) NOT NULL,
---                         PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE IF NOT EXISTS `comment` (
-    `id` bigint NOT NULL AUTO_INCREMENT,
-    ``
-)
+     `id` bigint NOT NULL AUTO_INCREMENT,
+     `created_date` datetime(6) NOT NULL,
+    `modified_date` datetime(6) NOT NULL,
+    `contents` longtext NOT NULL,
+    `artist_id` bigint NOT NULL,
+    `user_id` bigint NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+alter table comment
+    add constraint fk_comment_to_artist
+        foreign key (artist_id)
+            references artist(id);
+
+alter table comment
+    add constraint fk_comment_to_user
+        foreign key (user_id)
+            references user(id);
