efac1065647bb5ef1e94b1630fe846cbe133b2fa
joelonsw <joel610naver@gmail.com>
2022-11-05 17:40:57 +0900

    build: actuator 의존성 추가 및 springfox 충돌 해결

--- src/main/java/com/backend/connectable/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/backend/connectable/global/config/swagger/SwaggerConfig.java
@@ -1,9 +1,26 @@
 package com.backend.connectable.global.config.swagger;
 
+import static java.util.Collections.singletonList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,12 +33,6 @@ import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Predicate;
-
-import static java.util.Collections.singletonList;
-
 @Configuration
 public class SwaggerConfig {
 
@@ -31,43 +42,73 @@ public class SwaggerConfig {
     @Bean
     public Docket parseApi() {
         return new Docket(DocumentationType.OAS_30)
-            .ignoredParameterTypes(AuthenticationPrincipal.class)
-            .servers(serverInfo())
-            .apiInfo(apiInfo())
-            .globalRequestParameters(parameters())
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("com.backend.connectable"))
-            .paths(PathSelectors.ant("/**"))
-            .paths(Predicate.not(PathSelectors.regex("/admin.*")))
-            .build();
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .servers(serverInfo())
+                .apiInfo(apiInfo())
+                .globalRequestParameters(parameters())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.backend.connectable"))
+                .paths(PathSelectors.ant("/**"))
+                .paths(Predicate.not(PathSelectors.regex("/admin.*")))
+                .build();
     }
 
     private List<RequestParameter> parameters() {
         return singletonList(
-            new RequestParameterBuilder()
-                .name("Authorization")
-                .description("Required for user-based requests")
-                .in(ParameterType.HEADER)
-                .required(false)
-                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
-                .build()
-        );
+                new RequestParameterBuilder()
+                        .name("Authorization")
+                        .description("Required for user-based requests")
+                        .in(ParameterType.HEADER)
+                        .required(false)
+                        .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                        .build());
     }
 
     private Server serverInfo() {
-        return new Server("",
-            baseUrl,
-            "",
-            Collections.emptyList(),
-            Collections.emptyList()
-        );
+        return new Server("", baseUrl, "", Collections.emptyList(), Collections.emptyList());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("Connectable API Documentation")
-            .description("API documentation of Connectable project by Team UACC")
-            .version("1.0.0")
-            .build();
+                .title("Connectable API Documentation")
+                .description("API documentation of Connectable project by Team UACC")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(
+            WebEndpointsSupplier webEndpointsSupplier,
+            ServletEndpointsSupplier servletEndpointsSupplier,
+            ControllerEndpointsSupplier controllerEndpointsSupplier,
+            EndpointMediaTypes endpointMediaTypes,
+            CorsEndpointProperties corsProperties,
+            WebEndpointProperties webEndpointProperties,
+            Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping =
+                this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(
+                endpointMapping,
+                webEndpoints,
+                endpointMediaTypes,
+                corsProperties.toCorsConfiguration(),
+                new EndpointLinksResolver(allEndpoints, basePath),
+                shouldRegisterLinksMapping,
+                null);
+    }
+
+    private boolean shouldRegisterLinksMapping(
+            WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled()
+                && (StringUtils.hasText(basePath)
+                        || ManagementPortType.get(environment)
+                                .equals(ManagementPortType.DIFFERENT));
     }
 }
