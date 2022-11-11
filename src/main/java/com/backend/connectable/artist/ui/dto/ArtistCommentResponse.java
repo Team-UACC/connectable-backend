8ce1fe98f44e8ea8fa7917c478afa35f807fa614
mrlee7 <jplee227@gmail.com>
2022-11-11 10:49:37 +0900

    fix: writtenAt 타임 스탬프로 변경 (#168)
    Co-authored-by: jungpil98 <jungpil98@ajou.ac.kr>

--- src/main/java/com/backend/connectable/artist/ui/dto/ArtistCommentResponse.java
+++ src/main/java/com/backend/connectable/artist/ui/dto/ArtistCommentResponse.java
@@ -1,6 +1,8 @@
 package com.backend.connectable.artist.ui.dto;
 
+import com.backend.connectable.global.util.DateTimeUtil;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,11 +14,12 @@ public class ArtistCommentResponse {
 
     private String nickname;
     private String contents;
-    private LocalDateTime writtenAt;
+    private Long writtenAt;
 
+    @Builder
     public ArtistCommentResponse(String nickname, String contents, LocalDateTime writtenAt) {
         this.nickname = nickname;
         this.contents = contents;
-        this.writtenAt = writtenAt;
+        this.writtenAt = DateTimeUtil.toEpochMilliSeconds(writtenAt);
     }
 }
